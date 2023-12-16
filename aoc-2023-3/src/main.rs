78c5ef8795e42d5deef24671310f207a47af29f3
Guido Leeuwenstein <guidoleeuw@gmail.com>
2023-12-16 14:13:08 +0100

    wip on day 4

--- aoc-2023-3/src/main.rs
+++ aoc-2023-3/src/main.rs
@@ -1,43 +1,30 @@
-use regex::Regex;
-
-fn has_special_neighbor(metrix: &Vec<Vec<char>>, index: (usize, usize), len: usize) -> bool {
-    let re = Regex::new(r"\d|.").unwrap();
-    let mut serounding_chars: Vec<char> = Vec::new();
-    
-    if index.1 != 0 {
-        serounding_chars.push(metrix[index.0][index.1-1]);
-        serounding_chars.push(metrix[index.0+1][index.1-1]);
-        serounding_chars.push(metrix[index.0-1][index.1-1]);
-    };
-    if index.1 + len != 10 {
-        serounding_chars.push(metrix[index.0][index.1+len]);
-        serounding_chars.push(metrix[index.0+1][index.1+1]);
-        serounding_chars.push(metrix[index.0-1][index.1+1]);
-    }
-
-    if index.0 != 0 {
-        serounding_chars.append(metrix[index.0-1][index.1..(index.1+len)].to_vec().as_mut());
-    }
-    if index.0 != metrix.len() {
-        serounding_chars.append(metrix[index.0+1][index.1..(index.1+len)].to_vec().as_mut());
-    }
-
-    let str: String = serounding_chars.into_iter().collect();
-    return re.is_match(str.as_str());
-}
-
-fn main() {
-    let mut metrix: Vec<Vec<char>> = Vec::new();
-    let f = std::fs::read_to_string("./input.txt");
-    match f {
-        Ok(f) => {
-            for line in f.lines().into_iter() {
-                metrix.push(line.chars().collect());
-                for line in metrix {
-                    line.into_iter().collect::<String>().find(r"\d")
-                }
-            }
-        },
-        Err(er) => print!("{}", er)
-    }
+pub fn main() {
+    let map = include_bytes!("../input.txt");
+    let width = map.iter().position(|b| b == &b'\n').unwrap() as isize;
+    println!(
+        "{}",
+        (0..map.len() - 2)
+            .filter(|i| {
+                map[*i].is_ascii_digit()
+                    && !map.get(i.wrapping_sub(1)).map_or(false, u8::is_ascii_digit)
+            })
+            .map(|i| {
+                let d = (i + 1..i + 4)
+                    .position(|i| !map[i].is_ascii_digit())
+                    .unwrap()
+                    + 1;
+                (i, d as _, atoi::atoi::<usize>(&map[i..i + d]).unwrap())
+            })
+            .filter(|(i, d, _n)| {
+                (-width - 2..-width + *d)
+                    .chain([-1, *d])
+                    .chain(width..width + *d + 2)
+                    .any(|j| {
+                        map.get((*i as isize + j) as usize)
+                            .map_or(false, |b| b != &b'.' && b.is_ascii_punctuation())
+                    })
+            })
+            .map(|(_i, _d, n)| n)
+            .sum::<usize>()
+    );
 }
---
+++ aoc-2023-4/src/main.rs
@@ -0,0 +1,43 @@
+use core::panic;
+
+fn makeStrNumberVec (strs : &str) -> Vec<usize> {
+    let mut res: Vec<usize> = Vec::new();
+    let strNumbers = strs.split_whitespace().collect::<Vec<&str>>();
+    for number in strNumbers {
+        res.push(number.parse().unwrap())
+    }
+    return res;
+}
+
+fn main() {
+    let f = std::fs::read_to_string("./data.txt");
+    match f {
+        Ok(x) => {
+            let mut totalScore = 0;
+            for (matchId, line) in x.lines().enumerate() {
+                let mut score = 0;
+                let (_, numbers) = line.split_once(":").unwrap();
+
+                let (winning, mine ) = numbers.split_once("|").unwrap();
+                let mineParsed = makeStrNumberVec(mine);
+                let winningParsed = makeStrNumberVec(winning);
+                
+                for myNumber in mineParsed {
+                    if winningParsed.contains(&myNumber) {
+                        if score == 0  {
+                            score = 1
+                        } else {
+                            score = score * 2;
+                        }
+                    }
+                }
+                totalScore =totalScore + score;
+            }
+        print!("{}", totalScore);
+            return ()
+        }
+        Err(err) => {
+            panic!("{}",err);
+    }
+}
+}
